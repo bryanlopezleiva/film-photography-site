ff7c7697acf0bd4320371a0f52ee5a7202bc6b35
blopez <lopezz.bryann966@gmail.com>
2025-07-30 19:15:51 -0700

    Changes to header and footer. Included more images.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Photography Blog";
+export const SITE_TITLE = "Film Photography Blog";
 export const SITE_DESCRIPTION = "Welcome to my website!";
